cb720b8dfe8c0716af91b06f4a9fc9d3f2fd426e
shubhmgrg <shubham.garg@mail.utoronto.ca>
2025-07-07 16:35:13 -0400

    Added stripe sockets and unique user id players with tagger distinction

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+import { sequelize } from "../datasource.js";
+import { DataTypes } from "sequelize";
+
+export const User = sequelize.define("User", {
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  customerId: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  subscription: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: true,
+  }
+});
